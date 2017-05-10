316ec7ecee1f1ded7fdc744e160b08fc22551d30
jenearley <jen0earley@gmail.com>
2017-05-10 07:51:37 -0700

    Add files via upload

--- stopnwait.java
+++ stopnwait.java
@@ -1,10 +1,11 @@
 /* Stop and Wait  */
+import java.util.*;
 
 public class stopnwait
 {
-
     private int frameSize;
     private int packets;
+    private int waitTime = 10;
 
     public stopnwait(int packets, int frameSize)
     {
@@ -22,36 +23,48 @@ public class stopnwait
         this.frameSize = frameSize;
     }
 
-    public int getPacketsSize()
-    {
+    public int getPacketsSize(){
         return this.packets;
     }
 
-    public int sendFrames(network someNetwork, int time)
-    {
+    public int sendFrames(network someNetwork, int time){
         int bandwidthBPMS = someNetwork.getBandwidth();
-        for(int i = this.getFrameSize(); i>0; i-=bandwidthBPMS)
-        {
+        for(int i = this.getFrameSize(); i>0; i-=bandwidthBPMS){
             time++;
         }
         return time;
     }
 
-    public int waitForAck(network someNetwork, int time)
-    {
-        int bandwidthBPMS = someNetwork.getBandwidth();
-        double physicalDelay = (someNetwork.getHeight()/someNetwork.speedOfLight) * 1000.0;
-        int ackSendTime = 0;
-
-        for(int i = this.getFrameSize(); i>0; i-=bandwidthBPMS)
-        {
-            ackSendTime++;
+    public int waitForAck(network someNetwork, int time) {
+        Random rand = new Random();
+        int loss = rand.nextInt(20); //lost packet simulation
+        if(loss != packets){
+           int bandwidthBPMS = someNetwork.getBandwidth();
+            double physicalDelay = (someNetwork.getHeight()/someNetwork.speedOfLight) * 1000.0;
+            int ackSendTime = 0;
+            for(int i = this.getFrameSize(); i>0; i-=bandwidthBPMS){
+                ackSendTime++;
+            }
+            System.out.println("ACK SEND TIME: " + ackSendTime);
+            System.out.println("PHYSICAL DELAY: " + physicalDelay);
+            time = ackSendTime + (int) physicalDelay;
+            packets--;
+        } else {
+            time = 0;
         }
-
-        System.out.println(ackSendTime);
-        System.out.println(physicalDelay);
-        time = ackSendTime + (int) physicalDelay;
-
         return time;
+    }
+
+    public void multipleFrames(network someNetwork, int time, int frames){
+        int frameNum = 0;
+        while(frameNum < frames){
+            int sent = sendFrames(someNetwork, time);
+            System.out.println("Frame " + frameNum + " sent after " + sent);
+            int ack = waitForAck(someNetwork, time);
+            if(ack != 0){
+                System.out.println("ACK received for frame " + frameNum + " after " + ack);
+                frameNum++;
+            }
+        }
     }
 }
--- test.java
+++ test.java
@@ -1,34 +1,31 @@
-public class test
-{
-
-    public static void main(String[] args)
-    {
+public class test {
+    public static void main(String[] args) {
         System.out.println("PROTOCOL 4: STOP-AND-WAIT");
-
         stopnwait simpleProtocol4 = new stopnwait(1, 2400);
         network simpleNetwork = new network(40000.0, 56);
-
         int prot4time = 0;
-        for(int i = 0; i < simpleProtocol4.getPacketsSize(); i++)
-        {
+        for(int i = 0; i < simpleProtocol4.getPacketsSize(); i++){
             int sendFramesTime = simpleProtocol4.sendFrames(simpleNetwork, prot4time);
             System.out.println("SEND TIME: " + sendFramesTime);
-
             int ackTime = simpleProtocol4.waitForAck(simpleNetwork, prot4time);
             System.out.println("ACK TIME: " + ackTime);
-
             prot4time += (sendFramesTime + ackTime);
             System.out.println("STOP-AND-WAIT: " + prot4time);
         }
-
         System.out.println("Current time is : " + prot4time + "ms");
         System.out.println("--------------------------------------");
-
+        
         System.out.println("PROTOCOL 5: GO-BACK-N");
         gobackn simpleProtocol5 = new gobackn(5, 2400);
-
         simpleProtocol5.sendFrames(simpleNetwork);
         simpleProtocol5.waitForAck(simpleNetwork);
+
+        prot4time = 0;
+        System.out.println("--------------------------------------");
+        System.out.println("PROTOCOL 4: STOP-AND-WAIT");
+        stopnwait complexProtocol4 = new stopnwait(5, 2400);
+        complexProtocol4.multipleFrames(simpleNetwork, prot4time, 5);
+
 
     }
 }
